eb47ab63e61bd2ef570f713ce10b6959bd116267
Jalcubo <josedaniel3@hotmail.com>
2011-12-18 23:38:51 +0000

    Mas test

--- LPP/Practica_HTML/test/test_html.rb
+++ LPP/Practica_HTML/test/test_html.rb
@@ -2,20 +2,33 @@ require 'HTML'
 require 'test/unit'
 
 class Test_HTML < Test::Unit::TestCase
-	def test_inicial
-assert true ,HTML.new {html {
-            head(:dir => "chazam", :lang => "spanish") { title "My wonderful home page" }
-            body do
-              h1 "Welcome to my home page!", :class => "chuchu", :lang => "spanish"
-              b "My hobbies:"
-              ul do
-                li "Juggling"
-                li "Knitting"
-                li "Metaprogramming"
-              end #ul
-            end # body
-          }
-}	
-assert true , HTML.to_s 
+	
+	def test_sin_valores
+        	q = HTML.new {}
+        	assert_equal "", q.to_s
+        end	
+
+	def test_array_p
+                q = HTML.new {html 'prueba'}	
+		assert_equal 1, q.p.size
+		assert_equal ["<html>prueba</html>"], q.p[0]
+		assert_equal 1, q.p[0].size
+                assert_equal "<html>prueba</html>", q.p[0][0]
+        end
+	
+	def test_dimension_parrafo
+		q = HTML.new {html 'prueba'}
+		assert_equal 19, q.p[0][0].size
+	end
+
+	def test_valor_corto
+		q = HTML.new {html 'prueba'}
+		assert_equal "<html>prueba</html>", q.to_s 
+	end
+    
+        def test_sin_argumentos
+		assert_raises ArgumentError do
+			q = HTML.new
+		end
 	end
 end
