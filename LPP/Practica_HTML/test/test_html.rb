de64fd1b5eb22525999dda4cda54bfef98ac5f30
Jalcubo <josedaniel3@hotmail.com>
2011-12-18 16:23:00 +0000

    Completanto Rakefile

--- LPP/Practica_HTML/test/test_html.rb
+++ LPP/Practica_HTML/test/test_html.rb
@@ -1,7 +1,21 @@
+require 'HTML'
 require 'test/unit'
 
 class Test_HTML < Test::Unit::TestCase
-	def test_bye
-		assert true
+	def test_inicial
+assert true ,HTML.new {html {
+            head(:dir => "chazam", :lang => "spanish") { title "My wonderful home page" }
+            body do
+              h1 "Welcome to my home page!", :class => "chuchu", :lang => "spanish"
+              b "My hobbies:"
+              ul do
+                li "Juggling"
+                li "Knitting"
+                li "Metaprogramming"
+              end #ul
+            end # body
+          }
+}	
+assert true , HTML.to_s 
 	end
 end
